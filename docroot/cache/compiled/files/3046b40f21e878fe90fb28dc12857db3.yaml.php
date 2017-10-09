e418ee2838df1b18ee6546987c82fa660261c854
Felipe Molina Jimenez <arsjok.furhan@gmail.com>
2017-10-09 13:45:34 -0600

    LS-15: new branch with docker and grav

---
+++ docroot/cache/compiled/files/3046b40f21e878fe90fb28dc12857db3.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/user/config/security.yaml',
+    'modified' => 1507577339,
+    'data' => [
+        'salt' => 'xMSdl1p7oLhcEj'
+    ]
+];
---
+++ docroot/cache/compiled/files/97aad6a38e1d7fa65f3412b7595bbabb.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/var/www/html/user/data/licenses.yaml',
+    'modified' => 1507578083,
+    'data' => [
+        
+    ]
+];
